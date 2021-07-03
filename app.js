5521fcafc75a070f16d88f2efe6bfe713674288e
David Fuka <gravetotkd@gmail.com>
2021-07-02 20:55:11 -0700

    Generated HTML file

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
+const fs = require("fs");
+const generatePage = require("./src/page-template");
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, githubName] = profileDataArgs;
 
-const printProfileData = (profileDataArr) => {
-  for (let i = 0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i]);
-  }
-  console.log("===========================");
+// Code for demonstration purposes only
+// const printProfileData = (profileDataArr) => {
+//   for (let i = 0; i < profileDataArr.length; i++) {
+//     console.log(profileDataArr[i]);
+//   }
+//   console.log("===========================");
 
-  profileDataArr.forEach((element) => {
-    console.log(element);
-  });
-};
+//   profileDataArr.forEach((element) => {
+//     console.log(element);
+//   });
+// };
 
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
+// End demonstration code
+
+fs.writeFile("index.html", generatePage(name, githubName), (err) => {
+  if (err) throw new Error(err);
+  console.log("Portfolio complete! Check out index.html to see the output!");
+});
